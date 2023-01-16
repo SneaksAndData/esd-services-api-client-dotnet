6eebe5febd22dd1ad0333d5805fffaed2475539b
Vitaliy Savitskiy <savitskiy.vitaliy.m@gmail.com>
2023-01-16 14:04:53 +0100

    Update src/Config/BoxerConnectorOptions.cs
    Co-authored-by: George Zubrienko <zubrienkog@gmail.com>

--- src/Config/BoxerConnectorOptions.cs
+++ src/Config/BoxerConnectorOptions.cs
@@ -5,7 +5,7 @@ public class BoxerConnectorOptions
     /// <summary>
     /// Base URI of the boxer instance
     /// </summary>
-    public string? BaseUri { get; set; }
+    public string BaseUri { get; set; }
     
     /// <summary>
     /// Name of authorization provider
