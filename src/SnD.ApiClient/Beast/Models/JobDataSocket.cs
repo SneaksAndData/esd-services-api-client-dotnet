d2025204b05da30f7ed65abc09c1e0b5061b7646
Jakob Roar Bentzon <ext-jakb@ecco.com>
2024-02-12 19:24:08 +0100

    linting

--- src/SnD.ApiClient/Beast/Models/JobDataSocket.cs
+++ src/SnD.ApiClient/Beast/Models/JobDataSocket.cs
@@ -1,5 +1,3 @@
-using System.Text.Json.Serialization;
-
 namespace SnD.ApiClient.Beast.Models;
 
 public sealed class JobDataSocket
@@ -7,18 +5,15 @@ public sealed class JobDataSocket
     /// <summary>
     /// Alias of the data socket
     /// </summary>
-    [JsonPropertyName("alias")]
     public string Alias { get; set; }
     
     /// <summary>
     /// Fully qualified path to actual data, i.e. abfss://..., s3://... etc.
     /// </summary>
-    [JsonPropertyName("dataPath")]
     public string DataPath { get; set; }
     
     /// <summary>
     /// Data format, i.e. csv, json, delta etc.
     /// </summary>
-    [JsonPropertyName("dataFormat")]
     public string DataFormat { get; set; }
 }
