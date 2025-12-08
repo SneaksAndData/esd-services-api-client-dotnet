826e8f27036671ddea89e05e9e665f96f8f3e997
Vitalii Savitskii <visa@ecco.com>
2025-12-08 15:36:01 +0100

    Self-review fixes

--- test/SnD.ApiClient.Tests/MockServiceFixture.cs
+++ test/SnD.ApiClient.Tests/MockServiceFixture.cs
@@ -4,32 +4,30 @@ using Moq;
 using SnD.ApiClient.Boxer;
 using SnD.ApiClient.Boxer.Base;
 
-namespace SnD.ApiClient.Tests
-{
-    public class MockServiceFixture
-    {
-        public HttpClient CrystalMockHttpClient { get; }
-        public HttpClient BoxerMockHttpClient { get; }
+namespace SnD.ApiClient.Tests;
 
-        public MockServiceFixture()
-        {
-            CrystalMockHttpClient = new HttpClient(new CrystalMockHttpHandler());
-            BoxerMockHttpClient = new HttpClient(new BoxerMockHttpHandler());
-        }
-        
-        public Mock<HttpMessageHandler> GetMockedHttpClientHandler(Action<Mock<HttpMessageHandler>> setup)
-        {
-            var handlerMock = new Mock<HttpMessageHandler>();
-            setup(handlerMock);
-            return handlerMock;
-        }
-        
-        public Mock<IJwtTokenExchangeProvider> GetMockedJwtTokenExchangeProvider(Action<Mock<IJwtTokenExchangeProvider>> setup)
-        {
-            var jwtTokenExchangeProviderMock = new Mock<IJwtTokenExchangeProvider>();
-            setup(jwtTokenExchangeProviderMock);
-            return jwtTokenExchangeProviderMock;
-        }
+public class MockServiceFixture
+{
+    public HttpClient CrystalMockHttpClient { get; }
+    public HttpClient BoxerMockHttpClient { get; }
+
+    public MockServiceFixture()
+    {
+        CrystalMockHttpClient = new HttpClient(new CrystalMockHttpHandler());
+        BoxerMockHttpClient = new HttpClient(new BoxerMockHttpHandler());
     }
-    
+        
+    public Mock<HttpMessageHandler> GetMockedHttpClientHandler(Action<Mock<HttpMessageHandler>> setup)
+    {
+        var handlerMock = new Mock<HttpMessageHandler>();
+        setup(handlerMock);
+        return handlerMock;
+    }
+        
+    public Mock<IJwtTokenExchangeProvider> GetMockedJwtTokenExchangeProvider(Action<Mock<IJwtTokenExchangeProvider>> setup)
+    {
+        var jwtTokenExchangeProviderMock = new Mock<IJwtTokenExchangeProvider>();
+        setup(jwtTokenExchangeProviderMock);
+        return jwtTokenExchangeProviderMock;
+    }
 }
