e1875e2839b7d805f508d17e756fa9e240c71d9b
Vitalii Savitskii <visa@ecco.com>
2026-04-30 15:15:32 +0200

    Self-review fix

--- src/SnD.ApiClient/Boxer/BoxerAuthenticationProvider.cs
+++ src/SnD.ApiClient/Boxer/BoxerAuthenticationProvider.cs
@@ -4,13 +4,20 @@ using SnD.ApiClient.Boxer.Base;
 
 namespace SnD.ApiClient.Boxer;
 
-public class BoxerAuthenticationProvider(IJwtTokenExchangeProvider tokenProvider) : IAuthenticationProvider
+public class BoxerAuthenticationProvider: IAuthenticationProvider
 {
+    private readonly IJwtTokenExchangeProvider tokenProvider;
+
+    public BoxerAuthenticationProvider(IJwtTokenExchangeProvider tokenProvider)
+    {
+        this.tokenProvider = tokenProvider;
+    }
+    
     public async Task AuthenticateRequestAsync(RequestInformation request,
         Dictionary<string, object>? additionalAuthenticationContext = null,
         CancellationToken cancellationToken = default)
     {
-        var token = await tokenProvider.GetTokenAsync(true, cancellationToken);
+        var token = await this.tokenProvider.GetTokenAsync(true, cancellationToken);
         request.Headers.Add("Authorization", $"Bearer {token}");
     }
 }
